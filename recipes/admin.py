c12546dd7687c179413c484f17abad007c9add1c
Simon ANDRÉ <simonandre.strasbourg@protonmail.com>
2020-06-05 11:11:28 +0200

    add timedurationwidget and integrate it in ingredient admin form

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from django import forms
+from durationwidget.widgets import TimeDurationWidget
 from .models import IngredientUnit, IngredientType, Ingredient
 
+class IngredientAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(IngredientAdminForm, self).__init__(*args, **kwargs)
+        self.fields['conservation_time'].widget = TimeDurationWidget(
+            show_days=True, show_hours=False, show_minutes=False, show_seconds=False)
+
+class IngredientAdmin(admin.ModelAdmin):
+    form = IngredientAdminForm
+    fields = ['pub_date', 'ingredient_text', 'ingredient_type', 'ingredient_unit',
+              'conservation_time']
+
+admin.site.register(Ingredient, IngredientAdmin)
 
 admin.site.register(IngredientUnit)
 admin.site.register(IngredientType)
-admin.site.register(Ingredient)
